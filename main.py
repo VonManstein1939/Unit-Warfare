3c2b26e69ee5615e30d789a4283ce18971b21e5d
Xander <xandergergye@gmail.com>
2023-05-11 21:48:35 -0400

    init Main menu

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
-import pygame
+import pygame, sys
+
 pygame.init()
+
+screen = pygame.display.set_mode((715, 400))
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((100, 200))
+test_surface.fill(pygame.Color('black'))
+test_rect = test_surface.get_rect(center=(200, 250))
+class Button(pygame.sprite.Sprite):
+    def_
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.quit:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill((175, 215, 70))
+    screen.blit(test_surface, test_rect)
+
+    pygame.display.update()
+    clock.tick(60)
+    img = pygame.image.load("Start_button_for_email-removebg-preview.png")
